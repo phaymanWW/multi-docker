ad59cd2763af86704cef566781e3ef4321a34623
Paul.Hayman <Paul.Hayman@wwcPHAYMANmbp.local>
2019-10-07 18:44:27 -0400

    first commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = { 
+  redistHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGPASSWORD,
+  pgPassword: process.env.PGDATABASE,
+  pgPort: process.env.PGPORT
+};
